9675c4652828ab02f24a3feb91c65b1443519964
Atikur <atikurrahman7223@gmail.com>
2023-10-31 11:28:51 +0600

    fixed vercel deploy issue

--- index.js
+++ index.js
@@ -13,18 +13,24 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET}@cluster0.28gkq0d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+const client = new MongoClient(uri, { useUnifiedTopology: true }, { useNewUrlParser: true }, { connectTimeoutMS: 30000 }, { keepAlive: 1 });
+
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    client.connect();
+    client.connect((err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
 
     const database = client.db("collegeBooker");
     const collegeCollection = database.collection("colleges");
@@ -103,10 +109,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -121,3 +127,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`college booker server is running on port ${port}`);
 });
+
+// const client = new MongoClient(uri, { useUnifiedTopology: true }, { useNewUrlParser: true }, { connectTimeoutMS: 30000 }, { keepAlive: 1 });
